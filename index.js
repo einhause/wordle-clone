f3e7534c2a7bc503211f3ab60f1717d251d3c3b9
Eric Einhaus <eeinhaus17@gmail.com>
2022-02-08 19:09:32 -0500

    Fixed issue with duplicate letters being marked as worng location but should be wrong

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const keyboard = document.querySelector('[data-keyboard]');
 
 // the daily word to solve, from Wordle json file
 const dailyWord = getDailyWord();
+// get count of each of the letters of the dailyWord
+const dailyWordLetterCounts = getLetterCounts(dailyWord);
 
 // get daily word from targetWords array
 function getDailyWord() {
@@ -59,11 +61,13 @@ function handleMouseClick(e) {
     pressKey(e.target.dataset.key);
     return;
   }
+
   // enter key was pressed
   if (e.target.matches('[data-enter]')) {
     submitGuess();
     return;
   }
+
   // backspace/delete key
   if (e.target.matches('[data-delete]')) {
     deleteKey();
@@ -78,6 +82,7 @@ function handleKeyPress(e) {
     submitGuess();
     return;
   }
+
   // key pressed is backspace or delete key
   if (e.key === 'Backspace' || e.key === 'Delete') {
     deleteKey();
@@ -94,13 +99,15 @@ function handleKeyPress(e) {
 function pressKey(key) {
   // key press handler for a letter
   const activeTiles = getActiveTiles();
+
   // word can only be 5 letters, cannot exceed it
   if (activeTiles.length >= WORD_LENGTH) return;
+
   //selects next tile that does not have an user inputted letter
   const nextTile = guessGrid.querySelector(':not([data-letter])');
 
   // adds it to data object
-  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.dataset.letter = key;
   nextTile.dataset.state = 'active';
   nextTile.textContent = key;
 }
@@ -109,6 +116,7 @@ function deleteKey() {
   // delete key press handler
   const activeTiles = getActiveTiles();
   const lastTile = activeTiles[activeTiles.length - 1];
+
   // empty user input
   if (lastTile == null) return;
 
@@ -132,6 +140,7 @@ function getActiveTiles() {
 function submitGuess() {
   // convert querySelectorAll obj to an array
   const activeTiles = [...getActiveTiles()];
+
   // word is not long enough
   if (activeTiles.length !== WORD_LENGTH) {
     showAlert('Not enough letters');
@@ -145,39 +154,62 @@ function submitGuess() {
     ''
   );
 
+  const guessLetterCounts = getLetterCounts(guess);
+
+  // checks if the guess is a valid word in the dictionary
   if (!dictionary.includes(guess)) {
     showAlert('Not in word list');
     shakeTiles(activeTiles);
     return;
   }
 
+  // stop any user input
   stopInteraction();
-  activeTiles.forEach((...params) => flipTiles(...params, guess));
+
+  // flip tiles
+  activeTiles.forEach((...params) =>
+    flipTiles(...params, guess, guessLetterCounts)
+  );
 }
 
-function flipTiles(tile, index, array, guess) {
+function flipTiles(tile, index, array, guess, guessLetterCounts) {
+  // letter from the tile
   const letter = tile.dataset.letter;
+  // key on the keyboard
   const key = keyboard.querySelector(`[data-key="${letter}"i]`);
 
+  // 1st half of animation, flips 90deg up
   setTimeout(() => {
     tile.classList.add('flip');
   }, (index * FLIP_ANIMATION_DURATION) / 2);
 
+  // at the end of the transition, remove the flip class from tile and change color accordingly
   tile.addEventListener(
     'transitionend',
     () => {
       tile.classList.remove('flip');
       if (dailyWord[index] === letter) {
+        //correct letter and position
         tile.dataset.state = 'correct';
         key.classList.add('correct');
       } else if (dailyWord.includes(letter)) {
-        tile.dataset.state = 'wrong-location';
-        key.classList.add('wrong-location');
+        // correct letter, wrong location
+
+        // check for duplicates
+        if (guessLetterCounts[letter] > dailyWordLetterCounts[letter]) {
+          tile.dataset.state = 'wrong';
+          key.classList.add('wrong');
+        } else {
+          tile.dataset.state = 'wrong-location';
+          key.classList.add('wrong-location');
+        }
       } else {
+        // wrong letter and location
         tile.dataset.state = 'wrong';
         key.classList.add('wrong');
       }
 
+      // all tiles are filled in row, stop user input and check a win or loss
       if (index === array.length - 1) {
         tile.addEventListener(
           'transitionend',
@@ -194,8 +226,9 @@ function flipTiles(tile, index, array, guess) {
 }
 
 function checkWinOrLose(guess, tiles) {
+  // word guessed correctly, end the game
   if (guess === dailyWord) {
-    showAlert('You Win!', 5000);
+    showAlert('Great Work!', 5000);
     danceTiles(tiles);
     stopInteraction();
     return;
@@ -203,9 +236,11 @@ function checkWinOrLose(guess, tiles) {
 
   const remainingTiles = guessGrid.querySelectorAll(':not([data-letter])');
 
+  // if every tile is filled with a letter, the game will end
   if (remainingTiles.length === 0) {
-    showAlert(dailyWord.toUpperCase(), null);
+    showAlert(`Nice try! The word was: ${dailyWord.toUpperCase()}`, null);
     stopInteraction();
+    return;
   }
 }
 
@@ -264,6 +299,19 @@ function danceTiles(tiles) {
       );
     }, (index * DANCE_ANIMATION_DURATION) / 5);
   });
+}
+
+/* 
+========================
+  LETTER COUNT UTIL FUNCTION
+=======================
+*/
+function getLetterCounts(word) {
+  const counts = {};
+  for (let char of word) {
+    counts[char] = counts.hasOwnProperty(char) ? ++counts[char] : 1;
+  }
+  return counts;
 }
 
 /* 
